9c70764fc503613f722d7bcb49ec71b4e5eac2c5
TheDoctor12 <jkaskabas95@gmail.com>
2022-04-22 21:05:15 +0300

    aospa: Drop GoogleCameraGo
    Change-Id: Ibe352ab5ae9444106a5bf0b35f48dede445bb1c8
    
    # Conflicts:
    #	target/product/packages.mk

--- target/product/packages.mk
+++ target/product/packages.mk
@@ -12,10 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Camera
-PRODUCT_PACKAGES += \
-    GoogleCameraGo
-
 # curl
 PRODUCT_PACKAGES += \
     curl
