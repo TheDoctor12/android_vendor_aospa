59ee3f1b682515b631763ff4ad4c1f6a96f4779e
Michael Bestas <mkbestas@lineageos.org>
2023-03-21 20:27:55 +0200

    overlay: Import extra themes color bundles from TQ1A.221205.011
    * Extracted from cheetah TQ1A.221205.011 with the following:
      - adb shell cmd overlay dump com.android.systemui:accent
      - adb shell cmd overlay dump com.android.systemui:neutral
    
    Change-Id: I78505b539612c9af8e8ecd3a253ca3788564412f

--- overlay/overlays.mk
+++ overlay/overlays.mk
@@ -58,3 +58,6 @@ PRODUCT_PACKAGES += \
     IconPackRoundedSystemUIOverlay \
     IconPackRoundedThemePickerOverlay \
     ParanoidLauncherOverlay
+
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/aospa/overlay/static
+PRODUCT_PACKAGE_OVERLAYS += vendor/aospa/overlay/static
