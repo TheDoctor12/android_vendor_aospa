09adc1b71654274b4d644a1d7159def0be063a90
Sean Du <do4suki@gmail.com>
2026-08-16 13:33:00 +0800

    products: lmi: Disable legacy OMX service
    Set TARGET_SUPPORTS_OMX_SERVICE before aosp_base.mk is inherited.
    This stops base_vendor.mk from adding the legacy OMX service for
    devices shipping API level 33 or lower.
    
    Change-Id: I26f3881e139b2e2f69875762e1fab0078f64172b
    Signed-off-by: Sean Du <do4suki@gmail.com>

--- products/lmi/aospa_lmi.mk
+++ products/lmi/aospa_lmi.mk
@@ -5,6 +5,9 @@
 
 # Check for target product
 ifeq (aospa_lmi,$(TARGET_PRODUCT))
+ 
+# OMX
+TARGET_SUPPORTS_OMX_SERVICE := false
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
